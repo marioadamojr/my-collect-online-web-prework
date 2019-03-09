33a1a09ff7e75292c8895425362ec4dc7bf2d411
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-09 19:09:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+    new_array
+  else
+    "No block was given"
+  end
+end
 
 
